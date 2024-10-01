b41ad240442b232173319a81937f9684440b70d8
Abhishek k <bobbyyyyy.16@gmail.com>
2024-10-01 11:38:36 +0530

    setting up of cors

--- server/index.js
+++ server/index.js
@@ -7,15 +7,25 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// CORS options
+var corsOptions = {
+  origin: "https://names-generator-ai.vercel.app/",
+  optionsSuccessStatus: 200,
+};
+
+// Apply CORS middleware with options
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
+// Routes
 app.use("/api", gptRoutes);
 
 app.use("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Port configuration
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
