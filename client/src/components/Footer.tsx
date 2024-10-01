ec4bc2e69cd71f79e1830703b6e905cd6fa041f9
Abhishek k <bobbyyyyy.16@gmail.com>
2024-10-01 13:48:15 +0530

    Final prod

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,11 +3,12 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white shadow-md mt-8">
-      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-center items-center">
         <div className="text-gray-600 text-lg text-center">
           © {new Date().getFullYear()} Names Generator AI. All rights reserved.
         </div>
-        <div className="text-gray-600 text-lg text-center mt-2">
+        <span className="mx-4 text-gray-600">|</span>
+        <div className="text-gray-600 text-lg text-center">
           Made with ❤️ by{" "}
           <a
             href="https://github.com/bobbyy16"
--- client/src/components/NameGenerator.tsx
+++ client/src/components/NameGenerator.tsx
@@ -46,7 +46,7 @@ const NameGenerator: React.FC = () => {
   }, []);
 
   return (
-    <div className="w-100 mt-8 max-w-4xl mx-auto p-4 sm:w-90 sm:p-6 lg:p-8 bg-white shadow-md rounded-xl">
+    <div className="w-100 mt-8 max-w-4xl mx-auto sm:p-6 sm:pl-36 sm:pr-36 lg:p-8 bg-white shadow-md rounded-xl">
       <h1 className="text-3xl sm:text-4xl font-bold text-center mb-6 text-purple-700">
         Name Generator
       </h1>
@@ -90,7 +90,7 @@ const NameGenerator: React.FC = () => {
             {names.slice(0, -1).map((name, index) => (
               <li
                 key={index}
-                className="text-lg font-medium text-gray-700 bg-purple-100 p-3 rounded-md hover:bg-purple-200 transition-all duration-300 ease-in-out flex items-center justify-center"
+                className="text-md font-medium text-gray-700 bg-purple-100 p-3 rounded-md hover:bg-purple-200 transition-all duration-300 ease-in-out flex items-center justify-center"
               >
                 {name}
               </li>
