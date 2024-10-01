a068137fef33d0af6a9f99e3e6633edf98ebb4e2
Abhishek k <bobbyyyyy.16@gmail.com>
2024-10-01 13:39:14 +0530

    experienting with width in prod

--- client/src/components/NameGenerator.tsx
+++ client/src/components/NameGenerator.tsx
@@ -46,7 +46,7 @@ const NameGenerator: React.FC = () => {
   }, []);
 
   return (
-    <div className="w-full mt-8 max-w-4xl mx-auto p-4 sm:w-90 sm:p-6 lg:p-8 bg-white shadow-md rounded-xl">
+    <div className="w-100 mt-8 max-w-4xl mx-auto p-4 sm:w-90 sm:p-6 lg:p-8 bg-white shadow-md rounded-xl">
       <h1 className="text-3xl sm:text-4xl font-bold text-center mb-6 text-purple-700">
         Name Generator
       </h1>
